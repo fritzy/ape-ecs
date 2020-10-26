0b9fe21059f7547138c6bc2bace86f9d4f2e803e
Martin Emmert <martin@martinemmert.de>
2020-10-26 21:19:50 +0100

    chore(webbenchmark): update and fix browser benchmark

--- webbenchmark/index.js
+++ webbenchmark/index.js
@@ -1,38 +1,41 @@
 const CREATE = 500000;
-const ECS = require('../src/index');
+const ECS = require("../src/index.js");
 
 const descriptions = {
-  create2Comp: 'Create 50,000 entities with two simple components',
-  destroy2Comp: 'Destroy 50,000 entities with two simple components'
-}
+  create2Comp: "Create 50,000 entities with two simple components ",
+  destroy2Comp: "Destroy 50,000 entities with two simple components",
+  recreating: "Recreating components now that pool is established",
+  rewriteComp: "Changing the values of each component             ",
+};
 
 const times = {
   create2Comp: 0,
-  destroy2Comp: 0
+  destroy2Comp: 0,
+  recreating: 0,
 };
 
 function output(test) {
-
-  console.log(`${descriptions[test]}: ${(times[test]).toFixed(2)}ms`);
+  console.log(`${descriptions[test]}: ${times[test].toFixed(2)}ms`);
 }
+
+class Test extends ECS.Component {}
+Test.properties = {
+  a: 1,
+  b: 2,
+};
+
+class Test2 extends ECS.Component {}
+Test2.properties = {
+  c: 3,
+  d: 4,
+};
 
 function benchmarks() {
   let start, end;
 
-
-  const ecs = new ECS.World();
-  ecs.registerComponent('Test', {
-    properties: {
-      a: 1,
-      b: 2
-    }
-  });
-  ecs.registerComponent('Test2', {
-    properties: {
-      c: 3,
-      d: 4
-    }
-  });
+  const ecs = new ECS.World({ trackChanges: false, entityPool: 100 });
+  ecs.registerComponent(Test);
+  ecs.registerComponent(Test2);
 
   const entities = [];
 
@@ -41,43 +44,78 @@ function benchmarks() {
   start = performance.now();
 
   for (let i = 0; i < CREATE; i++) {
-
     entities.push(
       ecs.createEntity({
-        Test: {
-          a: 4,
-          b: 5
-        },
-        Test2: {
-          c: 6,
-          d: 7
-        }
+        components: [
+          {
+            type: "Test",
+            key: "Test",
+            a: 4,
+            b: 5,
+          },
+          {
+            type: "Test2",
+            key: "Test2",
+            c: 6,
+            d: 7,
+          },
+        ],
+      })
+    );
+  }
+
+  end = performance.now();
+  times.create2Comp = end - start;
+  output("create2Comp");
+
+  start = performance.now();
+  for (let i = 0; i < CREATE; i++) {
+    entities[i].c.Test.a = 14;
+    entities[i].c.Test.b = 15;
+    entities[i].c.Test2.c = 16;
+    entities[i].c.Test2.d = 17;
+  }
+  end = performance.now();
+  times.rewriteComp = end - start;
+  output("rewriteComp");
+
+  start = performance.now();
+  for (let i = 0; i < CREATE; i++) {
+    entities[i].destroy();
+  }
+  end = performance.now();
+  times.destroy2Comp = end - start;
+  output("destroy2Comp");
+
+  start = performance.now();
+  for (let i = 0; i < CREATE; i++) {
+    entities.push(
+      ecs.createEntity({
+        components: [
+          {
+            type: "Test",
+            lookup: "Test",
+            a: 4,
+            b: 5,
+          },
+          {
+            type: "Test2",
+            lookup: "Test2",
+            c: 6,
+            d: 7,
+          },
+        ],
       })
     );
   }
   end = performance.now();
-  times.create2Comp = end - start;
-  output('create2Comp');
-
-  start = performance.now();
-
-  for (let i = 0; i < CREATE; i++) {
-    entities[i].destroy();
-  }
-
-  end = performance.now();
-  times.destroy2Comp = end - start;
-  output('destroy2Comp');
-
+  times.recreating = end - start;
+  output("recreating");
 }
 
 function tick() {
-
   benchmarks();
-  console.log(times);
   setTimeout(tick, 3000);
 }
 
 tick();
-
-
